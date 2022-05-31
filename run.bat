3083bdfde2eb6600bf2b12305b487e10de5aa691
Jacob Obrien <jacobrien64@gmail.com>
2022-05-30 21:06:30 -0700

    added run.bat... it takes cares of the packages

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo OFF
+
+echo checking for pakages needing to be installed
+
+pip install -r requirements.txt
+
+echo =============DONE=============
+echo starting the program
+
+python3 main.py
